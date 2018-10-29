3d5638818754ca7958445d01c67a937cfee4fce2
Gaspare Ferraro <ferraro@gaspa.re>
2018-10-29 17:36:04 +0100

    circuit functions

--- project/3_circuit_omp.cpp
+++ project/3_circuit_omp.cpp
@@ -1,54 +1,84 @@
 #include <bits/stdc++.h>
+#include <omp.h>
+#include "clock.hpp"
 
-int op(int a, int b){ return a+b; }
+int op(int a, int b){ return a^b; }
+
+
+inline int K1(int t, int m){ return (1<<(m-t)); }
+inline int K2(int t){ return (1<<t) - 1; }
+
+std::tuple<int,int> G1(int t, int k)
+{
+  int l = (k*(1<<t)) - (1<<(t-1)) - 1;
+  int r = (k*(1<<t)) - 1;
+  return std::make_tuple(l, r);
+}
+
+std::tuple<int,int> G2(int t, int k, int m)
+{
+  int l = (k*(1<<(m-t))) - 1;
+  int r = (k*(1<<(m-t))) + (1<<(m-t-1)) - 1;
+  return std::make_tuple(l, r);
+}
 
 int main()
 {
 
-  int m = 8;
+  int m = 20;
   int n = 1<<m;
 
   std::vector<int> v(n);
   std::vector<int> v_seq(n);
   std::vector<int> v_par(n);
 
-  for(int i=0; i<n; i++) v[i] = 1;
+  for(int i=0; i<n; i++) v[i] = i;
 
+  auto start_time_seq = spm::timer::start();
   std::partial_sum(std::begin(v), std::end(v), std::begin(v_seq), op);
+  auto stop_time_seq = spm::timer::step(start_time_seq);
+  std::cout << "seq time " << stop_time_seq << std::endl;
 
+  // Omp parallel threads
+  omp_set_dynamic(0);
+  omp_set_num_threads(32);
+
+  auto start_time_tot = spm::timer::start();
 
   // Phase 1
   for(int t=1; t<=m; t++)
   {
-    int kl = (1<<(m-t));
+    auto start_time = spm::timer::start();
     #pragma omp parallel for schedule(static)
-    for(int k=1; k<=kl; k++)
+    for(int k=1; k<=K1(t, m); k++)
     {
-        int l = (k*(1<<t))-(1<<(t-1))-1;
-        int r = (k*(1<<t))-1;
-        v[r] = op(v[l], v[r]);
+      auto [l, r] = G1(t, k);
+      v[r] = op(v[l], v[r]);
     }
+    auto stop_time = spm::timer::step(start_time);
+    std::cout << "\tPHASE " << t << " " << stop_time << std::endl;
   }
-
 
   // Phase 2
   for(int t=1; t<m; t++)
   {
-    int kl = (1<<t)-1;
+    auto start_time = spm::timer::start();
     #pragma omp parallel for schedule(static)
-    for(int k=1; k<=kl; k++)
+    for(int k=1; k<=K2(t); k++)
     {
-        int l = (k*(1<<(m-t)))-1;
-        int r = (k*(1<<(m-t)))+(1<<(m-t-1))-1;
-        v[r] = op(v[l], v[r]);
+      auto [l, r] = G2(t, k, m);
+      v[r] = op(v[l], v[r]);
     }
+    auto stop_time = spm::timer::step(start_time);
+    std::cout << "\tPHASE " << t << " " << stop_time << std::endl;
   }
 
-  std::cout << std::endl;
-  for(int x : v) std::cout << x << " ";
-  std::cout << std::endl;
-  for(int x : v_seq) std::cout << x << " ";
-  std::cout << std::endl;
+  auto stop_time_tot = spm::timer::step(start_time_tot);
+  std::cout << "par time " << stop_time_tot << std::endl;
+
+  if( v != v_seq ) std::cout << "ERRORE!" << std::endl;
 
   return 0;
 }
+
+
